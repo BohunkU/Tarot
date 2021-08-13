bae6cf76c48e6585b91914a7f9ec8493e7c9a742
BohunkU <berente@DESKTOP-22VDKDG.localdomain>
2021-08-12 23:55:13 -0600

    finally got button on card, fetch data, but having problem displaying it

--- script.js
+++ script.js
@@ -1,44 +1,49 @@
-async function getChars () {
+async function getChars (url) {
 
-  let resObj = await fetch('https://rws-cards-api.herokuapp.com/api/v1/cards/');
-  let apiData = await resObj.json();
+  //let resObj = await fetch('https://rws-cards-api.herokuapp.com/api/v1/cards/');
+  let response = await fetch(url);
+  let data = await response.json();
   
-  console.log(apiData)
+  //console.log(apiData)
   
-  return apiData.results
-  }
+  return data
+}
   
-  let cardGrid = document.querySelector(".cardGrid");
+let cardGrid = document.querySelector(".cardGrid");
   
-  let startButton = document.querySelector("#startButton");
+let startButton = document.querySelector(".btn");
   
   // Mouse click
   
-  // startButton.addEventListener("click", () => {
-  //  loadPage();
-  // }); 
+startButton.addEventListener("click", () => {
+  loadPage();
+}); 
+
+function loadPage() {
+  getChars('https://rws-cards-api.herokuapp.com/api/v1/cards/random?n=3').then(async (data) => {
+    for (const card of data.cards) {
+      await getChars(card.url).then((cardData) => {
+      //  console.log(data)
+      //})
+      console.log(data)
+    }
+  })
+
+
+  
+}
   
   const rootDiv = document.querySelector('#root')
   
-  getChars().then((chars) => {
-    console.log(chars)
   
-    chars.forEach((char) => {
-      
-      let containerCharDiv = document.createElement('div')
   
-      let charHtml = `
-      
-          <h1>Name: name</h1>
-          <p>Key: meaning_up:</p>
-          <p>Description: desc</p>
-      `
-  
-      containerCharDiv.innerHTML = charHtml
-  
-      rootDiv.append(containerCharDiv);
-  
-    });
-  
-  });
-  
+
+ class Card {
+  constructor(name, meaning_up, name_short, desc) {
+    this.name = name;
+    this.meaning_up = meaning_up;
+    this.name_short = name_short;
+    this.desc = desc;
+    this.id = 1000
+  }
+}
